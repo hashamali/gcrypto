e25fca29bfc10b1be91966638d981e1902eb1d2d
Hasham Ali <the.hasham.ali@gmail.com>
2020-05-29 23:42:01 -0700

    Fix gosec.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # gcrypto
 [![tests](https://img.shields.io/github/workflow/status/hashamali/gcrypto/tests?label=tests&style=flat-square)](https://github.com/hashamali/gcrypto/actions?query=workflow%3Atests)
 [![sec](https://img.shields.io/github/workflow/status/hashamali/gcrypto/security?label=security&style=flat-square)](https://github.com/hashamali/gcrypto/actions?query=workflow%3Asecurity)
-[![coverage](https://badgen.net/codecov/c/github/hashamali/gcrypto)](https://codecov.io/gh/hashamali/gcrypto)
+[![coverage](https://img.shields.io/codecov/c/github/hashamali/gcrypto)](https://codecov.io/gh/hashamali/gcrypto)
 [![go-report](https://goreportcard.com/badge/github.com/hashamali/gcrypto)](https://goreportcard.com/report/github.com/hashamali/gcrypto)
 [![license](https://badgen.net/github/license/hashamali/gcrypto)](https://opensource.org/licenses/MIT)
 
--- gcrypto.go
+++ gcrypto.go
@@ -2,28 +2,48 @@ package gcrypto
 
 import (
 	"crypto/hmac"
-	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
+
+	/* #nosec */
+	"crypto/md5"
 )
 
 // HMACSHA256 returns a SHA-256 of the given message signed by the secret.
-func HMACSHA256(secret string, message string) string {
+func HMACSHA256(secret string, message string) (string, error) {
+	if secret == "" {
+		return "", errors.New("secret must not be empty")
+	}
+
 	h := hmac.New(sha256.New, []byte(secret))
-	h.Write([]byte(message))
-	return hex.EncodeToString(h.Sum(nil))
+	_, err := h.Write([]byte(message))
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
 
 // SHA256 returns a SHA-256 of the given message.
-func SHA256(message string) string {
+func SHA256(message string) (string, error) {
 	h := sha256.New()
-	h.Write([]byte(message))
-	return hex.EncodeToString(h.Sum(nil))
+	_, err := h.Write([]byte(message))
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
 
 // MD5 returns the MD5 checksum of a given message.
-func MD5(message string) string {
+func MD5(message string) (string, error) {
+	/* #nosec */
 	h := md5.New()
-	h.Write([]byte(message))
-	return hex.EncodeToString(h.Sum(nil))
+	_, err := h.Write([]byte(message))
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
