7527cb015e49f6f9372eb6c1544cca930777e8d2
Hasham Ali <the.hasham.ali@gmail.com>
2020-05-29 23:33:02 -0700

    Update and cleanup.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-## gcrypto
-
-[![CircleCI](https://circleci.com/gh/hashamali/gcrypto/tree/master.svg?style=svg)](https://circleci.com/gh/hashamali/gcrypto/tree/master)
+# gcrypto
+[![tests](https://img.shields.io/github/workflow/status/hashamali/gcrypto/tests?label=tests&style=flat-square)](https://github.com/hashamali/gcrypto/actions?query=workflow%3Atests)
+[![sec](https://img.shields.io/github/workflow/status/hashamali/gcrypto/security?label=security&style=flat-square)](https://github.com/hashamali/gcrypto/actions?query=workflow%3Asecurity)
+[![coverage](https://badgen.net/codecov/c/github/hashamali/gcrypto)](https://codecov.io/gh/hashamali/gcrypto)
+[![go-report](https://goreportcard.com/badge/github.com/hashamali/gcrypto)](https://goreportcard.com/report/github.com/hashamali/gcrypto)
+[![license](https://badgen.net/github/license/hashamali/gcrypto)](https://opensource.org/licenses/MIT)
 
 Thin wrapper around Go's standard crypto library.
 
--- gcrypto_test.go
+++ gcrypto_test.go
@@ -7,18 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSHA256(t *testing.T) {
-	sha1 := SHA256("input1")
-	assert.Equal(t, strings.ToLower("1EA06586B18E8FCE1B923EFF26FD8252F617F0EFD4E49820E8E9BEE0614E5792"), sha1)
+func TestHMACSHA256(t *testing.T) {
+	secret := "secret"
+	digest := HMACSHA256(secret, "input1")
+	assert.Equal(t, "b985131d6c4a56d40dece6e6413bf4b29abe48c0431d265f0ceec89a52e0f6b8", digest)
 
-	sha2 := SHA256("input2")
-	assert.Equal(t, strings.ToLower("124D8541FF3D7A18B95432BDFBECD86816B86C8265BFF44EF629765AFB25F06B"), sha2)
+	digest = HMACSHA256(secret, "input2")
+	assert.Equal(t, "3e23a4b3f4246e49fb896778297c4d600a2c5430392a1201af8f20617a070005", digest)
+}
+
+func TestSHA256(t *testing.T) {
+	digest := SHA256("input1")
+	assert.Equal(t, strings.ToLower("1EA06586B18E8FCE1B923EFF26FD8252F617F0EFD4E49820E8E9BEE0614E5792"), digest)
+
+	digest = SHA256("input2")
+	assert.Equal(t, strings.ToLower("124D8541FF3D7A18B95432BDFBECD86816B86C8265BFF44EF629765AFB25F06B"), digest)
 }
 
 func TestMD5(t *testing.T) {
-	md51 := MD5("input1")
-	assert.Equal(t, strings.ToLower("da853c5826b798b82320e42024d97837"), md51)
+	digest := MD5("input1")
+	assert.Equal(t, strings.ToLower("da853c5826b798b82320e42024d97837"), digest)
 
-	md52 := MD5("input2")
-	assert.Equal(t, strings.ToLower("3eaa25d43fac6e39a12c3936942b72c8"), md52)
+	digest = MD5("input2")
+	assert.Equal(t, strings.ToLower("3eaa25d43fac6e39a12c3936942b72c8"), digest)
 }
